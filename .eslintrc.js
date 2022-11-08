57e66f15bfeca960c25f634fc3b176fa55b20fb4
Bablu Ahmed <bablukpik@gmail.com>
2022-11-08 22:58:51 +0600

    master: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    "object-curly-newline": ["error", {
+      "ObjectExpression": { "multiline": true, "minProperties": 3 }, // object
+      "ObjectPattern": { "multiline": true, "minProperties": 2 }, // object destructuring
+      "ImportDeclaration": { "multiline": true, "minProperties": 2 },
+      "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+    }],
+    "array-element-newline": ["error", {
+      "ArrayExpression": { "multiline": true, "minItems": 3 }, // array
+      "ArrayPattern": { "multiline": true, "minItems": 2 }, // array destructuring
+    }],
+    'react/jsx-props-no-spreading': 'off',
+    'max-len': ['error', {
+      code: 120,
+      ignoreComments: true,
+      ignoreUrls: true,
+    }],
+  },
+};
